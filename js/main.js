38cece0bf576cbcc79743a14c3c836eb7715b3f1
FadyZa <92400077+FadyZa@users.noreply.github.com>
2022-10-23 16:59:15 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
+// GET HTML ELEMENTS
+let aside = document.querySelector(".container aside");
+let temp = document.getElementById("tempc")
+let city = document.getElementById("city");
+let condition = document.getElementById("condition");
+let date = document.getElementById("date");
+let wind = document.querySelector(".wind");
+let humidity = document.querySelector(".humidity");
+let err = document.getElementById("err");
+
+/////////////////////////////////////////////////////
 document.querySelector("form button").addEventListener("click",(e)=>{
     e.preventDefault()
-    console.log("clicked")
+    // clear any previous styles with smooth transitions
     window.setTimeout(()=>{
         aside.style.opacity = 0;
         aside.style.transform = "scale(0)";
-    },200)
+
+    },200);
     document.getElementById("err").style.display = "none";
     document.querySelector(".container aside").style.display = "none";
-    console.log( document.querySelector(".container aside"));
 
+    // regular exp to make sure that user entered a valid city without numbers or Chars
     let regex = /\b[^\d\W]+\b/gi;
     let city = document.querySelector("form input").value;
 
@@ -18,49 +30,77 @@ document.querySelector("form button").addEventListener("click",(e)=>{
             let Data = await fetch(api);
             let JsData = await Data.json();
             callFunc(JsData);        
-        }
-        getData(handlData);
+        };
+        // a call back function send data to a check function to check validation before show it to the user
+        getData(checkData);
     }
-    else{
-        handleErr("please enter a valid city")
+    // make sure again and again that the user entered a valid city noty empty input
+    else if (city == ""){
+        handleErr("please Enter Your Location");
+    }
+    else {
+        handleErr("please Enter A Valid Location")
     }
 });
 
-let aside = document.querySelector(".container aside");
-let temp = document.getElementById("tempc")
-let city = document.getElementById("city");
-let condition = document.getElementById("condition");
-let date = document.getElementById("date");
-let wind = document.querySelector(".wind");
-let humidity = document.querySelector(".humidity");
 
+/////////////////////////////////////////////////////
+// validation checker for the data
+function checkData(myData) {
+    // check if there are any errors
+    if (myData.error){
+        if(myData.error.code == 1006){
+            handleErr(myData.error.message) // means no location found with user input
+        }
+        else if(myData.error.code == 2008){
+            handleErr("Sorry! Can Not Connect To Server ") // there is error with the API KEY
+        }
+        else if (myData.error.code == 1005){
+            handleErr(myData.error.message) // API key is invalid or not provided.
+        }
+        else if (myData.error.code == 1002){
+            handleErr(myData.error.message) // API URL is invalid.
+        }
+        else{
+            handleErr("Unknown Erorr!")
+        }
+    }
+    else {
+        handlData(myData);
+    }
+};
 
+/////////////////////////////////////////////////////
+// HANDLE ERRORS
 function handlData(myData){
-    aside.style.display = "block"; 
-    window.setTimeout(()=>{
-        aside.style.opacity = 1;
-        aside.style.transform = "scale(1)";
-        aside.classList.add("animate__animated","animate__fadeInTopRight")
-    },400)
-    console.log(myData.location.region);
+    // fill the html elements with the valid data
     temp.innerHTML = `${myData.current.temp_c}<sup>&#8451;</sup>`;
     city.innerText = myData.location.region;
     condition.innerHTML =`${myData.current.condition.text} <img width="35px" alt="weather condition" src="${myData.current.condition.icon}">`
     date.innerText = myData.current.last_updated;
     wind.innerText = `${myData.current.wind_kph} KM/H`;
-    humidity.innerText = `${myData.current.humidity}%`
-}
+    humidity.innerText = `${myData.current.humidity}%`   
+    aside.style.display = "block"; 
 
-let err = document.getElementById("err");
+    // make some animation and transition with the result show
+    window.setTimeout(()=>{
+        aside.style.opacity = 1;
+        aside.style.transform = "scale(1)";
+        aside.classList.add("animate__animated","animate__fadeInTopRight")
+    },400);
+};
+
+/////////////////////////////////////////////////////
+// HANDLE ERRORS
+// handle html element to show the error message
 function handleErr(text){
-    err.style.display = "block";
+    // fill the html elements with the valid data
     err.innerHTML = text;
+    err.style.display = "block";
+    // make some animation and transition with the error show
     window.setTimeout(()=>{
         err.style.opacity = 1;
         err.style.transform = "scale(1)";
-    },0)
-}
-
-
-
-
+        err.classList.add("animate__animated","animate__tada")
+    },100)
+};
